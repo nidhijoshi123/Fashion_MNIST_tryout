d89bf133fa736eb71bd6fe75a33d3e744f1bfe37
nidhijoshi123 <nidhi.nj.joshi@fau.de>
2020-02-04 18:06:08 +0100

    Update Testing_testSET.py

--- Testing_testSET.py
+++ Testing_testSET.py
@@ -6,18 +6,24 @@ import Supporting_functions
 import torch
 from sklearn.metrics import confusion_matrix
 
-
+#Load trained model
 model = torch.load('C:/Users/nidhi/FashionMNIST/model/model.pt')
 
+#Load entire test dataset at once
 prediction_loader=torch.utils.data.DataLoader(Data_preparation.test_set,batch_size=10000)
+
+#Save predictions on test dataset
 test_preds=Supporting_functions.get_all_preds(model,prediction_loader)
 
+#Keep grad disabled for less memory
 with torch.no_grad():
     prediction_loader= torch.utils.data.DataLoader(Data_preparation.test_set,batch_size=10000)
     test_preds=Supporting_functions.get_all_preds(model,prediction_loader)
-    
+ 
+#Total correct predictions on the test dataset
 preds_correct= Supporting_functions.get_num_correct(test_preds,Data_preparation.test_set.targets)
 
+#Accuracy on the test dataset
 print("total correct predictions (test set): ",preds_correct)
 print("Accuracy (test set): ", (preds_correct/len(Data_preparation.test_set))*100,"%")
 
